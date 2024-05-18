4b8c68d92f561362a62cae5d4977e61754c76e34
Timo Schneider <timoschneider04@gmail.com>
2024-05-18 03:29:32 +0200

    DerAStASollSichFickenUndNichtSelbstZerlegen43

--- src/models/base.rs
+++ src/models/base.rs
@@ -54,10 +54,10 @@ impl Base {
         let mut pop = base.population_in_n_ticks(d, game_config, attacks);
 
         if base.uid == 0 && base.level == 4 {
-            pop = 0;
+            pop = 1;
         }
         else {
-            pop += 1;
+            pop += 3;
         }
 
         if d < game_config.paths.grace_period {return pop}
